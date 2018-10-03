69baa4ebc423cebd48598b02191651e4564b6618
hsuchiyi <hsuchiyi@gmail.com>
2018-10-03 21:51:06 +0800

    average

--- analysis.py
+++ analysis.py
@@ -6,3 +6,7 @@ with open('reviews.txt', 'r') as f:
         count = count + 1
         if count % 1000 == 0:
             print(len(data))
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print('留言的平均長度為', sum_len/len(data))
